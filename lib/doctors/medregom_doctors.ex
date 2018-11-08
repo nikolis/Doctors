35bca903e48968d099db6c8a99b2f559c739b2da
root <root>
2018-11-08 17:44:37 +0200

    latest changes

--- lib/doctors/medregom_doctors.ex
+++ lib/doctors/medregom_doctors.ex
@@ -1,14 +1,19 @@
 defmodule Doctors.MedregomDoctors do 
   
   def fetch(first_name, last_name) do
-    issues_url(first_name, last_name)
+    doctor_query_url(first_name, last_name)
     |> HTTPoison.post("",[{"Content-Type", "application/json"}])
     |> handle_response
     |> validate_names_using_query_results(first_name, last_name)
   end
 
-  def issues_url(firstName, lastName) do
-    "https://www.medregom.admin.ch/EN/Suche/GetSearchData?name=#{String.trim(lastName)}&Vorname=#{firstName}"
+  def doctor_query_url(first_name, last_name) do
+    URI.to_string(%URI{
+      scheme: "https", 
+      host: "www.medregom.admin.ch",
+      path: "/EN/Suche/GetSearchData", 
+      query: "name=#{last_name}&Vorname=#{first_name}"
+      })
   end
 
   def handle_response({_, %{status_code: status, body: body}})do
@@ -19,7 +24,6 @@ defmodule Doctors.MedregomDoctors do
       |> handle_doctor_details_extraction()
       |> extract_first_and_last_name()
   end
-
 
   def validate_names_using_query_results(r_list, f_name, l_name) do
     {f_name, l_name} in r_list 
